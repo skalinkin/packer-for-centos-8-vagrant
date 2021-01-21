4ff2fd8a71a34082425935db195d982ef51e7fc0
Sergiy Kalinkin <skalinkin@gmail.com>
2021-01-20 22:55:53 -0500

    add rsync, increase timeout, 2021.20.0

--- provision.sh
+++ provision.sh
@@ -5,6 +5,3 @@ mkdir /mnt/VBoxGuestAdditions
 mount -o loop VBoxGuestAdditions.iso /mnt/VBoxGuestAdditions
 sh /mnt/VBoxGuestAdditions/VBoxLinuxAdditions.run --nox11
 umount /mnt/VBoxGuestAdditions
-
-# install usefull packages
-dnf install -y wget cockpit
